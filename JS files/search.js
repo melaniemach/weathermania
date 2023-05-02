7dbfddc0e62d16a05e0d9cc5f7fe2bf920e35042
Melanie Mach <melaniemach@csu.fullerton.edu>
2023-05-01 22:26:08 -0700

    Updates
    added icons, changed some colors around

--- JS files/search.js
+++ JS files/search.js
@@ -3,6 +3,28 @@ const weatherForm = document.getElementById('weather-form');
 const resultList = document.getElementById('result-list');
 const apiKey = '840a3ddffcd9ac4065f9837e8e4cf233';
 
+// Weather icons
+const iconMap = {
+  '01d': '01d.png', //clear-day
+  '01n': '01n.png', //clear-night
+  '02d': '02d.png', //partly-cloudy-day
+  '02n': '02n.png', //partly-cloudy-night
+  '03d': '03d.png', //cloudy
+  '03n': '03n.png', //cloudy
+  '04d': '04d.png', //cloudy
+  '04n': '01d.png', //cloudy
+  '09d': '09d.png', //rain
+  '09n': '01d.png', //rain
+  '10d': '10d.png', //rain
+  '10n': '10n.png', //rain
+  '11d': '11d.png', //thunderstorm
+  '11n': '11n.png', //thunderstorm
+  '13d': '13d.png', //snow
+  '13n': '13n.png', //snow
+  '50d': '50d.png', //fog
+  '50n': '50n.png', //fog
+}
+
 // Add a listener for the form submit event
 weatherForm.addEventListener('submit', function(event) {
   event.preventDefault(); // prevent form submission
@@ -28,6 +50,8 @@ weatherForm.addEventListener('submit', function(event) {
       const humidity = data.main.humidity;
       const wind = data.wind.speed;
       const timezone = data.timezone;
+      const weatherConditionCode = data.weather[0].icon;
+      const iconUrl = `../Images/${data.weather[0].icon}.png`;
       
       // Use the timezone to get the current time
       const currentTime = timezone ? new Date(Date.now() + (timezone * 1000)) : new Date();
@@ -41,6 +65,7 @@ weatherForm.addEventListener('submit', function(event) {
       newItem.innerHTML = `
        <div class="card">
          <h2 class="card-title">${city}</h2>
+         <img src="${iconUrl}">
          <div class="card-body">
             <p>Current Temperature: ${temp}&deg;F</p>
             <p>Feels Like: ${feelsLike}&deg;F</p>
